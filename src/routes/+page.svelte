9c9f3eeaaa6abf2f4ec2e82de0bcfc6fa25c5608
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-06-22 22:49:54 +0900

    breeze-ish colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<h1>Huan-gîng sú-iōng SvelteKit</h1>
-<p>
-  Tshiánn pài-hóng <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> lâi tha̍k bûn-kiānn
-</p>
-<p>(test)</p>
+<div class="prose">
+  <h1>Huan-gîng sú-iōng SvelteKit</h1>
+  <p>
+    Tshiánn pài-hóng <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> lâi tha̍k bûn-kiānn
+  </p>
+  <p>(test)</p>
+</div>
