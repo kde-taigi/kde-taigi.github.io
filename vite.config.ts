392e2eae645cc096fca72284785458148f666415
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-06-22 22:13:42 +0900

    adapter-static

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
@@ -8,7 +8,7 @@ export default defineConfig({
     tailwindcss(),
     sveltekit({
       compilerOptions: {
-        // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+        // Force runes mode for the project, except for libraries
         runes: ({ filename }) =>
           filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
       },
