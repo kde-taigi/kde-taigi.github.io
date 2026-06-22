6d9d0001cbd390861a06dac875ea24fd625ad0be
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-06-22 22:27:16 +0900

    scaffold header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,15 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<header>
+  <nav class="p-2">
+    <ul class="flex gap-1">
+      <li><a href="/">頭頁</a></li>
+      <li><a href="/about">紹介</a></li>
+      <li><a href="/glossary">用詞</a></li>
+    </ul>
+  </nav>
+</header>
+<main class="px-2">
+  {@render children()}
+</main>
