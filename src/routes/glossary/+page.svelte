597902cbd2de1d6dd16d060551a4bb478f40dd97
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-06-24 01:01:14 +0900

    Attempt to parse with a nicer (????) format
    Fighting Zod and TypeScript is hell.
    
    Why does .find(type === "element" && name === "text") (pseudocode) not
    narrow to "undefined or a matching object"? Who knows.

--- src/routes/glossary/+page.svelte
+++ src/routes/glossary/+page.svelte
@@ -1,5 +1,37 @@
+<script lang="ts">
+  import terms from "$lib/assets/terms.json?raw";
+  import { Terms } from "$lib";
+  const parsed = Terms.parse(JSON.parse(terms));
+  const textElem = parsed.root.children.find(
+    (it) => it.type === "element" && it.name.raw === "text",
+  );
+</script>
+
 <div class="prose">
   <p>TODO</p>
   <p>我會囥我選 ê 用詞 kap 解說來遮。</p>
   <p>用詞這馬我佇 <a href="https://www.terminologue.org/z9m89rsek/">Terminologue</a> 管理。</p>
 </div>
+
+<!--
+{#each entries as entry}
+  <div class="border">
+    {#each entry.langSet as langSet}
+      <div class="flex">
+        <div class="min-w-[4rem]">
+          {langSet["@_lang"]}
+        </div>
+        <div>
+          {#each ensureArray(langSet.ntig) as ntig}
+            {#each ensureArray(ntig.termGrp.term) as term}
+              <div>
+                {term}
+              </div>
+            {/each}
+          {/each}
+        </div>
+      </div>
+    {/each}
+  </div>
+{/each}
+-->
